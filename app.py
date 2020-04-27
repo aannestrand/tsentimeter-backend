7640078c89416a5366e69cd40e0b3aa1a84ab980
aannestrand <aannestrand98@gmail.com>
2020-04-27 12:40:04 -0500

    Added app to main dir

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 from flask import Flask
-from scripts.twitter import store_tweets
+from app.scripts.twitter import store_tweets
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)

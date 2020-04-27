6b987ad25a8807763ba7000a0749a7f9c3ae79aa
aannestrand <aannestrand98@gmail.com>
2020-04-27 00:55:29 -0500

    Trying

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    application.run(debug=True)
